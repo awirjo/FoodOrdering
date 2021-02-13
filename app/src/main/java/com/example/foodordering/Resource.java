eaa0b7856f4727d5ca13a54ac7869d8993eb876a
awirjo <65690626+awirjo@users.noreply.github.com>
2021-02-13 00:00:49 -0300

    Initial commit

---
+++ app/src/main/java/com/example/foodordering/Resource.java
@@ -0,0 +1,31 @@
+package com.example.foodordering;
+
+public class Resource {
+    private String name;
+    private String description;
+    private int imageResouceId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImageResouceId() {
+        return imageResouceId;
+    }
+
+    public void setImageResouceId(int imageResouceId) {
+        this.imageResouceId = imageResouceId;
+    }
+}
